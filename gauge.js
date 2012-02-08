d90f93e40cc1352cca5bf30d5d07f50c9af98be8
Hawken Rives <hawkrives@gmail.com>
2012-02-08 12:30:49 -0600

    Add gaug.es tracking

---
+++ gauge.js
@@ -0,0 +1,11 @@
+var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4f32bc35f5a1f52aa200000f');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
